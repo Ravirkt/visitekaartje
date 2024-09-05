2bc43ef317b91e8d859a873d9767e902c548cef2
Ravirkt <r_tjikhoeri@live.com>
2024-09-05 18:34:07 +0200

    added darkmode with javascript

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,47 @@
 //   document.body.classList.toggle('expand')
 // }
 
-function test() {
-  document.body.style.background = 'black';
-}
+// const test = document.querySelector('img');
+
+// test.addEventListener('click',)
+
+// console.log(test);
+
+
+// document.getElementById('foto').onclick 
+// const testingbutton = document.getElementById('button');
+// const infoblock = document.getElementById('foto');
+
+// function changecolor(event) {
+//    console.log(event);
+//   event.target.style.filter = 'none';
+
+// }
+
+
+
+// infoblock.addEventListener("click", changecolor);
+//   event.target.style.backgroundColor = 'black'
+
+
+
+
+
+  // document.getElementsByClassName('info').style.backgroundColor = 'green';
+
+
+  // testingbutton.textContent = 'hdfsdf'
+  // console.log('jfsjdfs');
+  
+  // const foto = document.getElementById('foto');
+  // const card = document.querySelector('.card-inner');
+
+ 
+
+const button1 = document.getElementById("button1");
+const body = document.body;
+
+button1.addEventListener("click", function() {
+body.classList.toggle("dark-mode");
+  
+});
